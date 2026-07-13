d52444f69ce8319a94a0d4ffb94ecbe9383a9b99
pafulleren <b34777692-wq@gmail.com>
2026-07-13 12:15:10 +0700

    i have no idea what the fuck im doing

--- template.lisp
+++ template.lisp
@@ -17,49 +17,6 @@
       :background-color "light-dark(#fafafa, #1b262c)"
       :color "light-dark(#000000, #eeeeee)"
       :font-size "1.25em"))
-    (.fnref
-     :position "relative"
-     :cursor "pointer"
-     ("a" :text-decoration "none")
-     (".fn-preview"
-      :display "none"
-      :position "absolute"
-      :bottom "1.4em"
-      :left "50%"
-      :transform "translateX(-50%)"
-      :width "20em"
-      :background "light-dark(#fafafa, #1b262c)"
-      :color "light-dark(#000000, #eeeeee)"
-      :border "1px solid #ccc"
-      :border-radius "4px"
-      :padding ".5em .75em"
-;      :font-size "1em"
-      :font-weight "normal"
-      :vertical-align "baseline"
-      :z-index 10
-      :box-shadow "0 2px 8px rgba(0,0,0,.15)")
-     ((:parent :hover)
-      (.fn-preview
-       :display block)))
-    (:media "(hover: none)"
-     (.fnref
-      ((:parent :hover)
-       (.fn-preview
-        :display none)))
-     ((.footnotes li)
-      :display none
-      ((:parent :target)
-       :display block
-       :position fixed
-       :bottom 0
-       :left 0
-       :right 0
-       :background "light-dark(#fafafa, #1b262c)"
-       :border-top "2px solid #ccc"
-       :padding "1em 1.25em"
-       :z-index 100
-       :font-size "1rem"
-       :box-shadow "0 -4px 12px rgba(0,0,0,.2)")))
     (.title
      ((:parent -container)
       :display flex
@@ -83,7 +40,33 @@
       :margin 0
       :font-size "2rem"
       :color "light-dark(#1a1a1a, a1a1a1)"
-      :line-height 1.2))))
+      :line-height 1.2))
+    (.footnote-modal-overlay
+     :position "absolute"
+     :bottom "1.4em"
+     :left "50%"
+     :transform "translateX(-50%)"
+     :width "50vw"
+     :background "light-dark(#f8f8f8, #19242a)"
+     :color "light-dark(#000000, #eeeeee)"
+     :border "1px solid #ccc"
+     :border-radius "4px"
+     :padding ".5em .75em"
+     ;:font-size "0.75em"
+     :font-weight "normal"
+     :vertical-align "baseline"
+     :z-index 1000
+     :box-shadow "0 2px 8px rgba(0,0,0,.15)"
+     :height "fit-content")
+    (:media "(hover: none)"
+            (.footnote-modal-overlay
+             :display "flex"
+             :justify-content "center"
+             :position "fixed"
+             :bottom 0
+             :width "100vw"))
+    (.hidden
+     :display "none")))
 
 (defstruct blog title date body)
 
@@ -105,14 +88,43 @@
          (:sup.fnref
            (:a :href (format nil "#fn-~a" ,n)
                :id  (format nil "fnref-~a" ,n)
-             (format nil "[~a]" ,n)
-             (:span.fn-preview ,@content)))))))
+               :_ (format nil "on mouseenter
+                                 put innerHTML of #fn-~a into #footnote-popup-box then
+                                   remove .hidden from #footnote-popup-box then
+                                   if (window.matchMedia('hover: hover').matches)
+                                   measure #footnote-popup-box then set :popheight to result.height
+                                   then set *top of #footnote-popup-box to (event.clientY - :popheight) + 'px'
+                                   then set *left of #footnote-popup-box to (event.clientX - 25) + 'px'
+                                 end
+                               end
+                               on mouseout wait 100ms then if (not $isTooltipHovered) add .hidden to #footnote-popup-box end end
+                               on click
+                                 put innerHTML of #fn-~0@*~a into #footnote-popup-box then
+                                 remove .hidden from #footnote-popup-box
+                               end" ,n)
+               (format nil "[~a]" ,n)))))))
 
 (defmacro with-footnote-context (&body body)
   `(let ((*footnotes* nil)
          (*footnote-counter* 0))
      ,@body
-     (render-footnotes)))
+     (render-footnotes)
+     (spinneret:with-html
+      (:div :id "footnote-popup-box"
+            :class "footnote-modal-overlay hidden"
+            :_ "on click from body
+                  if event.target is not in me and not event.target.matches('.fnref a')
+                    add .hidden to me
+                  end
+                end
+                on keyup from window
+                  if event.key is 'Escape' or event.target.id is 'footnote-popup-box'
+                    add .hidden to me
+                  end
+                end
+                on mouseenter remove .hidden from me then set $isTooltipHovered to true end
+                on mouseleave wait 150ms then add .hidden to me then set $isTooltipHovered to false end"
+            :style "position: fixed;"))))
 
 (defun title->slug (title)
   (string-downcase
@@ -130,7 +142,8 @@
         (:header :class "title-container"
                  (:a :href "/" (:h1 :class "title-text" (blog-title blog)))
                  (:span :class "title-date" (blog-date blog)))
-        (funcall (blog-body blog)))))))
+        (funcall (blog-body blog))
+        (:script :src "https://cdn.jsdelivr.net/npm/hyperscript.org@0.9.93/dist/_hyperscript.min.js" :integrity "sha384-/6HsqTiz02YfFBUhzTwlH/yxe68DhfnkdHiWytM3nxAzs/yvG+3FZY0f4KLnNoov" :crossorigin "anonymous"))))))
 
 (defun make-page (blog)
   (let* ((title (blog-title blog))
